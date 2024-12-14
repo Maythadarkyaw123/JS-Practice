545750a2a44de07d704aa745fdc08c3e129ccbac
May Thadar Kyaw <maythada.dku@gmail.com>
2024-12-14 16:06:59 +0530

    Chapter 1

---
+++ 1.4.js
@@ -0,0 +1,6 @@
+let a = 10; //a value is 10
+console.log(a); // this is printing a value in console
+
+// this
+// is 
+// comment
---
+++ ch1-pj.js
@@ -0,0 +1,3 @@
+console.log("May Thadar Kyaw") //multi-line comment
+let name = prompt("Enter your name: ")
+alert(name + ", You are now in practice!\nYour random number is "+ Math.random()*100);
