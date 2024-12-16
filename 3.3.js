cb06d70762a0d2249f59cc0ac6dbb64d03e2f2df
May Thadar Kyaw <maythada.dku@gmail.com>
2024-12-16 18:23:53 +0530

    Array

---
+++ 3.3.js
@@ -0,0 +1,11 @@
+let originalArray = [1, 2, 3];
+console.log(originalArray);
+
+
+newArray = [originalArray, originalArray, originalArray]
+console.log(newArray);
+
+console.log(newArray[0][1]);
+
+
+
