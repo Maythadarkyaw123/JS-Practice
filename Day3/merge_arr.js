58676a215e74bb2159f6e311a882b705b010e774
May Thadar Kyaw <maythada.dku@gmail.com>
2025-02-13 13:17:21 +0530

    Day 3

---
+++ Day3/merge_arr.js
@@ -0,0 +1,4 @@
+let arr1 = ['may', 'thadar', 'kyaw'];
+let arr2 = ['eaint', 'mue', 'aung'];
+let merge_arr = [...arr1 ,...arr2];
+console.log(merge_arr);
---
+++ Day3/remove_duplicate.js
@@ -0,0 +1,4 @@
+let myarr = [1,3,5,3,4,5,6,7];
+let unique = new Set(myarr);
+let arr = [...unique]
+console.log(arr);
